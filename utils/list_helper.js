18a28f3159b5c219ddb013fd0e51907b5935cbf7
Alisa <alisa.luoma@hotmail.fi>
2023-06-15 14:09:54 +0300

    task 4.6

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,4 +1,6 @@
-const dummy = (blogs) => {
+const _ = require('lodash')
+
+const dummy = () => {
   return 1
 }
 
@@ -8,12 +10,37 @@ const totalLikes = (blogs) => {
   return blogs.reduce(reduceLikes , 0)
 }
 
-const favoriteBlog = (blogs) = {
-  
+const favoriteBlog = (blogs) => {
+  const mostLikes = (favoriteBlog, compareBlog) => {
+    return (compareBlog.likes > favoriteBlog.likes) ? compareBlog : favoriteBlog
+  }
+
+  const mostfavorite = blogs.reduce(mostLikes, { likes:0 })
+
+  const blog =  {
+    title: mostfavorite.title,
+    author: mostfavorite.author,
+    likes: mostfavorite.likes
+  }
+
+  return blog
+}
+
+const mostBlogs = (blogs) => {
+  const blogsAuthor = _.groupBy(blogs, 'author')
+  const authorMostBlogs = _.maxBy(Object.keys(blogsAuthor), (author) => blogsAuthor[author].length)
+
+  const blog =  {
+    author: authorMostBlogs,
+    blogs: blogsAuthor[authorMostBlogs].length
+  }
+
+  return blog
 }
 
 module.exports = {
   dummy,
   totalLikes,
-  favoriteBlog
+  favoriteBlog,
+  mostBlogs
 }
