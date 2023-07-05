f0e9e47a6fbc8a964d2dfc184379e34660eba37b
Alisa <alisa.luoma@hotmail.fi>
2023-07-05 12:35:57 +0300

    Adding data for task 5.8

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -2,32 +2,33 @@ const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({}).find({}).populate('user', { username: 1, name: 1, id: 1 })
+  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
   response.json(blogs)
 })
 
 blogsRouter.post('/', async (request, response) => {
-  const body = request.body
-
-  if(!body.title || !body.url) {
-    return response.status(400).json({ error: 'Missing title or url' })
-  }
+  const { title, author, url, likes } = request.body
 
   const user = request.user
 
+  if (!user) {
+    return response.status(401).json({ error: 'operation not permitted' })
+  }
+
   const blog = new Blog({
-    title: body.title,
-    author: body.author,
-    url: body.url,
-    likes: body.likes,
-    user: user._id
+    title, author, url,
+    likes: likes ? likes : 0
   })
+
+  blog.user = user._id
 
   const savedBlog = await blog.save()
   user.blogs = user.blogs.concat(savedBlog._id)
   await user.save()
 
-  response.status(201).json(savedBlog)
+  const newBlog = await Blog.findOne({ _id: savedBlog._id }).populate('user', { username: 1, name: 1 })
+
+  response.status(201).json(newBlog)
 })
 
 blogsRouter.delete('/:id', async (request, response) => {
